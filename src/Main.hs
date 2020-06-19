28a6a6e9cb85986e5ef40620aee364333a08ed5e
Chua Hou <human+github@chuahou.dev>
2020-06-19 16:17:55 +0800

    Main: add bot API skeleton

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,34 @@
+-- SPDX-License-Identifier: MIT
+-- Copyright (c) 2020 Chua Hou
+
 module Main where
 
+import Telegram.Bot.API ( Token, Update, defaultTelegramClientEnv )
+import Telegram.Bot.Simple
+
+import Xkcdify
+
+type Model = ()
+data Action = Reply String | Noop deriving Show
+
+handleUpdate :: Update -> Model -> Maybe Action
+handleUpdate _ _ = Nothing
+
+handleAction :: Action -> Model -> Eff Action Model
+handleAction _ _ = undefined
+
+xkcdbot :: BotApp Model Action
+xkcdbot = BotApp
+  { botInitialModel = ()
+  , botAction = handleUpdate
+  , botHandler = handleAction
+  , botJobs = []
+  }
+
+run :: Token -> IO ()
+run token = do
+  env <- defaultTelegramClientEnv token
+  startBot_ xkcdbot env
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = getEnvToken "TELEGRAM_BOT_TOKEN" >>= run
